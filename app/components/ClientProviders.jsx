413ad1eb740916d0cb28b96861e60e6e0af4f38a
Vatsu919 <vatsunp11@gmail.com>
2025-12-25 12:55:55 +0530

    Fixed import issue

--- app/components/ClientProviders.jsx
+++ app/components/ClientProviders.jsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import ForegroundMessageListener from "./components/ForegroundMessageListener";
-import PushInitializer from "./components/PushInitializer";
-import RegisterSW from "./components/RegisterSW";
+import ForegroundMessageListener from "../components/ForegroundMessageListener";
+import PushInitializer from "../components/PushInitializer";
+import RegisterSW from "../components/RegisterSW";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
